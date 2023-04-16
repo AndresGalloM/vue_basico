33068511709212f9e39a6e722c8401e5b6ccf923
AndresGalloM <felipegallom1011@gmail.com>
2023-04-16 12:34:58 -0500

    v-bind: esta directiva nos permite hacer que tanto el atributo como su valor sean reactivos

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      img: 'https://fastly.picsum.photos/id/363/200/300.jpg?hmac=LvonEMeE2QnwxULuBZW5xHtdjkz844GnAPpEhDwGvMY'
     }
   },
-  template: `<div v-text="message"></div>`
+  template: `<img v-bind:src="img" v-bind:alt="img"></img>`
 }).mount('#app')
