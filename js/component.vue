f07eebb792a126b9ecd5a53647aefcfc8c29e5ed
AndresGalloM <felipegallom1011@gmail.com>
2023-04-23 19:52:03 -0500

    Ejemplo de como manejar sigle file components

---
+++ js/component.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Single file component</p>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hola vue'
+            }
+        },
+    }
+</script>
+
+<style>
+    p {
+        cursor: pointer;
+    }
+</style>
